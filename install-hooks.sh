299fb4812ba8be4c79eb95e3eaa814711ed59ef0
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2014-08-02 21:33:49 +0200

    Improve install-hooks.sh
    /install-hooks.sh:
    Now we change to the repository root before copying the hooks.

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# change to the root directory of the repository
+cd "$(git rev-parse --show-toplevel)"
+
+# install hooks
 ln -s ../../hooks/lines.py \
     ../../hooks/changedfiles.py \
     ../../hooks/pre-commit \
